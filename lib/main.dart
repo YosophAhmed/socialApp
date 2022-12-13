44bb2f91630439d3f508274e2ce28b6e049d8e2f
Yosoph Ahmed <devyosoph@gmail.com>
2022-12-13 16:11:37 +0200

    Adding Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 import 'package:social_app/screens/auth/login/login_screen.dart';
 import 'package:social_app/screens/auth/register/register_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const SocialApp(),
   );
