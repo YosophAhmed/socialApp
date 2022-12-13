b7dd466560851913057b0caf66ff1312b19fc442
Yosoph Ahmed <devyosoph@gmail.com>
2022-12-13 15:11:26 +0200

    Login screen Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sizer/sizer.dart';
-import 'package:social_app/screens/login/cubit/cubit.dart';
 import 'package:social_app/screens/login/login_screen.dart';
 
 void main() {
@@ -15,21 +13,18 @@ class SocialApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (BuildContext context) => LoginCubit(),
-      child: Sizer(builder: (context, orientation, deviceType) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          initialRoute: LoginScreen.routeName,
-          routes: {
-            LoginScreen.routeName: (context) => const LoginScreen(),
-          },
-          theme: ThemeData(
-            brightness: Brightness.dark,
-            fontFamily: 'Roboto',
-          ),
-        );
-      }),
-    );
+    return Sizer(builder: (context, orientation, deviceType) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: LoginScreen.routeName,
+        routes: {
+          LoginScreen.routeName: (context) => const LoginScreen(),
+        },
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Roboto',
+        ),
+      );
+    });
   }
 }
--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -19,7 +19,6 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   @override
   void initState() {
     SystemChrome.setSystemUIOverlayStyle(
@@ -32,86 +31,92 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const CustomTopImage(
-              imageName: 'assets/images/social.png',
-            ),
-            BlocBuilder<LoginCubit, LoginState>(
-              builder: (context, state) {
-                return Form(
-                  key: BlocProvider.of<LoginCubit>(context).formKey,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10.w),
-                    child: Column(children: [
-                      SizedBox(
-                        height: 10.h,
-                      ),
-                      CustomTextFormField(
-                        controller: BlocProvider.of<LoginCubit>(context).emailController,
-                        hintText: 'Email',
-                        onChanged: (value) {},
-                        onSubmitted: (value) {},
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                      SizedBox(
-                        height: 3.h,
-                      ),
-                      CustomTextFormField(
-                        controller: BlocProvider.of<LoginCubit>(context).passwordController,
-                        hintText: 'Password',
-                        onChanged: (value) {},
-                        onSubmitted: (value) {},
-                        keyboardType: TextInputType.visiblePassword,
-                        suffix: BlocProvider.of<LoginCubit>(context).suffix,
-                        obscure: BlocProvider.of<LoginCubit>(context).obscure,
-                        suffixFunction: () {
-                          BlocProvider.of<LoginCubit>(context).changePasswordVisibility();
-                        },
-                      ),
-                      SizedBox(
-                        height: 3.h,
-                      ),
-                      CustomButton(
-                        label: 'Log in',
-                        onTap: () {},
-                      ),
-                      SizedBox(
-                        height: 1.h,
-                      ),
-                      TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Forgot Password?',
-                          style: TextStyle(
-                              fontSize: 12.sp,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.blueAccent),
+    return BlocProvider(
+      create: (BuildContext context) => LoginCubit(),
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const CustomTopImage(
+                imageName: 'assets/images/social.png',
+              ),
+              BlocBuilder<LoginCubit, LoginState>(
+                builder: (context, state) {
+                  return Form(
+                    key: BlocProvider.of<LoginCubit>(context).formKey,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.w),
+                      child: Column(children: [
+                        SizedBox(
+                          height: 10.h,
                         ),
-                      ),
-                      SizedBox(
-                        height: 4.h,
-                      ),
-                      const CustomDivider(
-                        text: ' OR ',
-                      ),
-                      SizedBox(
-                        height: 4.h,
-                      ),
-                      CustomButton(
-                        label: 'Create new Social account',
-                        onTap: () {},
-                        buttonColor: Colors.green,
-                        buttonWidth: 60.w,
-                      ),
-                    ]),
-                  ),
-                );
-              },
-            ),
-          ],
+                        CustomTextFormField(
+                          controller: BlocProvider.of<LoginCubit>(context)
+                              .emailController,
+                          hintText: 'Email',
+                          onChanged: (value) {},
+                          onSubmitted: (value) {},
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        SizedBox(
+                          height: 3.h,
+                        ),
+                        CustomTextFormField(
+                          controller: BlocProvider.of<LoginCubit>(context)
+                              .passwordController,
+                          hintText: 'Password',
+                          onChanged: (value) {},
+                          onSubmitted: (value) {},
+                          keyboardType: TextInputType.visiblePassword,
+                          suffix: BlocProvider.of<LoginCubit>(context).suffix,
+                          obscure: BlocProvider.of<LoginCubit>(context).obscure,
+                          suffixFunction: () {
+                            BlocProvider.of<LoginCubit>(context)
+                                .changePasswordVisibility();
+                          },
+                        ),
+                        SizedBox(
+                          height: 3.h,
+                        ),
+                        CustomButton(
+                          label: 'Log in',
+                          onTap: () {},
+                        ),
+                        SizedBox(
+                          height: 1.h,
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Forgot Password?',
+                            style: TextStyle(
+                                fontSize: 12.sp,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blueAccent),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 4.h,
+                        ),
+                        const CustomDivider(
+                          text: ' OR ',
+                        ),
+                        SizedBox(
+                          height: 4.h,
+                        ),
+                        CustomButton(
+                          label: 'Create new Social account',
+                          onTap: () {},
+                          buttonColor: Colors.green,
+                          buttonWidth: 60.w,
+                        ),
+                      ]),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
